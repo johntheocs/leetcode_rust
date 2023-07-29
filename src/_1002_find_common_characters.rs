befc3f6d913783c90d6242dd215ad40f7199c49a
John Friend <115430128+johntheocs@users.noreply.github.com>
2023-07-29 16:43:43 -0400

    wrong solution. need to include duplicated characters as well.

--- src/_1002_find_common_characters.rs
+++ src/_1002_find_common_characters.rs
@@ -7,7 +7,28 @@
 // @lc code=start
 impl Solution {
     pub fn common_chars(words: Vec<String>) -> Vec<String> {
-        
+        use std::collections::{HashSet,HashMap};
+
+        let mut map : HashMap::<String, i8> = HashMap::<String, i8>::new();
+        let words_len = words.len() as i8;
+
+        for word in words {
+            let mut char_set: HashSet::<char> = HashSet::<char>::new();
+            for c in word.chars() {
+                if char_set.contains(&c) {
+                    continue;
+                } else {
+                    char_set.insert(c);
+                }
+            }
+            for ch in char_set {
+                let c = ch.to_string();
+                let count = map.get(&c).unwrap_or(&0) + 1;
+                map.insert(c, count);
+            }
+        }
+
+        map.iter().filter(|(_key, value) | **value == words_len as i8).map(|(key, _value)| key.clone()).collect()
     }
 }
 // @lc code=end
